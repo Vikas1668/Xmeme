a1934b887e188f57fb8d068be6e2ced58030e5de
administrator <vchoudhary1668@gmail.com>
2022-08-06 04:50:30 +0530

    Remove @EnableWebMvc

--- src/main/java/com/crio/starter/App.java
+++ src/main/java/com/crio/starter/App.java
@@ -6,9 +6,7 @@ package com.crio.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 public class App {
 
